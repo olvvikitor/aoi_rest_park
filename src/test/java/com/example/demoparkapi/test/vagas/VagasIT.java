51d0b02648e9c4f48495b47c877b179a45950f24
olviktor <126020776+olviktor@users.noreply.github.com>
2023-11-27 23:38:55 -0300

    adcionado teste unitario para consulta de vagas por codigo

--- src/test/java/com/example/demoparkapi/test/vagas/VagasIT.java
+++ src/test/java/com/example/demoparkapi/test/vagas/VagasIT.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.example.demoparkapi.dtos.VagaCreateDto;
-import com.example.demoparkapi.exceptions.ErrorMessage;
+import com.example.demoparkapi.dtos.VagaResponseDto;
 import com.example.demoparkapi.test.jwt.JwtAuthentication;
 
 
@@ -33,14 +33,51 @@ public class VagasIT {
 			.expectHeader().exists(org.springframework.http.HttpHeaders.LOCATION);
 			}
 		@Test
-		public void criar_vagas_existente_com_perfil_valido_retorna_status_409() {	
-			ErrorMessage error=
-					testVagas.get().uri("/api/v1/vagas")
-					.headers(JwtAuthentication.getHeaderAuthorization(testVagas, "joao@gmail.com", "123456")) 
-					.exchange().expectStatus().isEqualTo(409)
-					.expectBody(ErrorMessage.class)
-					.returnResult().getResponseBody();
-			org.assertj.core.api.Assertions.assertThat(error).isNotNull();
-			org.assertj.core.api.Assertions.assertThat(error.getStatus()).isEqualTo(409);
+		public void criar_vagas_que_ja_existe_com_perfil_valido_retorna_status_409() {	
+			testVagas.post().uri("/api/v1/vagas")
+			.contentType(MediaType.APPLICATION_JSON)
+			.headers(JwtAuthentication.getHeaderAuthorization(testVagas, "joao@gmail.com", "123456"))
+			.bodyValue(new VagaCreateDto ("01-A","LIVRE"))
+			.exchange()
+			.expectStatus().isEqualTo(409)
+			.expectBody()
+			.jsonPath("status").isEqualTo(409)
+			.jsonPath("method").isEqualTo("POST");
+			}
+		
+		@Test
+		public void criar_vagas_dados_invalidos_perfil_valido_retorna_status_422() {	
+			testVagas.post().uri("/api/v1/vagas")
+			.contentType(MediaType.APPLICATION_JSON)
+			.headers(JwtAuthentication.getHeaderAuthorization(testVagas, "joao@gmail.com", "123456"))
+			.bodyValue(new VagaCreateDto ("01-UI","DESOCUPADO"))
+			.exchange()
+			.expectStatus().isEqualTo(422)
+			.expectBody()
+			.jsonPath("status").isEqualTo(422)
+			.jsonPath("method").isEqualTo("POST");
+		}
+		@Test
+		public void buscar_vagas_com_dados_validos() {	
+			VagaResponseDto vaga = testVagas.get().uri("/api/v1/vagas/01-A")
+			.headers(JwtAuthentication.getHeaderAuthorization(testVagas, "joao@gmail.com", "123456"))
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody(VagaResponseDto.class)
+			.returnResult().getResponseBody();
+			
+			org.assertj.core.api.Assertions.assertThat(vaga.getId()).isEqualTo(1);
+			org.assertj.core.api.Assertions.assertThat(vaga.getCodigo()).isEqualTo("01-A");			
+		}
+		
+		@Test
+		public void criar_vagas_dados_invalidos_perfil_valido_retorna_status_404() {	
+			testVagas.get().uri("/api/v1/vagas/0-A")
+			.headers(JwtAuthentication.getHeaderAuthorization(testVagas, "joao@gmail.com", "123456"))
+			.exchange()
+			.expectStatus().isEqualTo(404)
+			.expectBody()
+			.jsonPath("status").isEqualTo(404)
+			.jsonPath("method").isEqualTo("GET");
 		}
 }
